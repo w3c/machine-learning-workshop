962c951335ab71ce424dafcc8a83a4d8815079b6
Anssi Kostiainen <anssi.kostiainen@gmail.com>
2020-08-24 10:15:23 +0300

    Fix TypeError occurring when submit.js is on a page w/o a form

--- submit.js
+++ submit.js
@@ -1,30 +1,35 @@
 /*jshint esversion: 6 */
 (() => {
-    document.querySelector('#submit-button').onclick = () => {
-        const name = document.querySelector('#speaker-name').value;
-        const bio = document.querySelector('#speaker-bio').value;
-        const title = document.querySelector('#talk-title').value;
-        const abstract = document.querySelector('#talk-abstract').value;
+    const button = document.querySelector('#submit-button');
+    if (button) addSubmitButtonListener();
 
-        const subject = `[workshop proposal] ${title}`;
-        const body = `Hi Program Committee,` +
-        `\n\n` +
-        `I would like to submit a talk proposal for the W3C Workshop on Web and Machine Learning.` +
-        `\n\n` +
-        `Name: ${name}` +
-        `\n\n` +
-        `Bio: ${bio}` +
-        `\n\n` +
-        `Talk title: ${title}` +
-        `\n\n` +
-        `Talk abstract: ${abstract}`+
-        `\n\n` +
-        `Best regards,\n${name}`;
+    function addSubmitButtonListener() {
+        document.querySelector('#submit-button').onclick = () => {
+            const name = document.querySelector('#speaker-name').value;
+            const bio = document.querySelector('#speaker-bio').value;
+            const title = document.querySelector('#talk-title').value;
+            const abstract = document.querySelector('#talk-abstract').value;
 
-        window.location.href =
-            'mailto:group-machine-learning-pc@w3.org' +
-            '?subject=' + encodeURIComponent(subject) +
-            '&body=' + encodeURIComponent(body);
-    };
+            const subject = `[workshop proposal] ${title}`;
+            const body = `Hi Program Committee,` +
+            `\n\n` +
+            `I would like to submit a talk proposal for the W3C Workshop on Web and Machine Learning.` +
+            `\n\n` +
+            `Name: ${name}` +
+            `\n\n` +
+            `Bio: ${bio}` +
+            `\n\n` +
+            `Talk title: ${title}` +
+            `\n\n` +
+            `Talk abstract: ${abstract}`+
+            `\n\n` +
+            `Best regards,\n${name}`;
+
+            window.location.href =
+                'mailto:group-machine-learning-pc@w3.org' +
+                '?subject=' + encodeURIComponent(subject) +
+                '&body=' + encodeURIComponent(body);
+        };
+    }
 })();
   
