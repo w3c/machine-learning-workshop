7dd8b2d2d8e5070d3e0c4bfa81e1079022a543f6
ruoxiran <ran@w3.org>
2020-08-28 15:07:52 +0800

    add Chinese translation into json file (#89)
    * fixed the captions of expression
    
    * Use template in translated Chinese pages
    
    * Make presentations page use translated data on talks
    
    Co-authored-by: Dominique Hazael-Massieux <dom@w3.org>

--- _tools/generate_talk_pages.js
+++ _tools/generate_talk_pages.js
@@ -7,56 +7,67 @@ const toSlug = title => title.replace(/([A-Z])/g, s => s.toLowerCase())
 
 let sectionNum = 0;
 let prevlink = "", prevtitle = "";
+const languages = {"en": "", "zh-hans": "_zh"};
+const translatable = ["abstract", "affiliation", "bio", "title"];
+
 for (let section of talks) {
   sectionNum++;
   const shortnames = Object.keys(section);
-  let sectionContent = `<div class="talks">` ;
-for(let i = 0; i < shortnames.length; i++) {
-  const shortname = shortnames[i];
-  const talk = section[shortname];
-  const slug = toSlug(talk.title)
-  if (talk.video) {
-    const duration = talk.duration.split(':')[0].replace(/^0/, '');
+  const sectionContent = {"en": `<div class="talks">`, "zh-hans": `<div class="talks">`};
+  for(let i = 0; i < shortnames.length; i++) {
+    const shortname = shortnames[i];
+    const origTalk = section[shortname];
+    const slug = toSlug(origTalk.title);
+    for (let [lang, suffix] of Object.entries(languages)) {
+      const talk = {...origTalk};
+      for (let key of translatable) {
+        if (suffix && origTalk[key + suffix]) {
+          talk[key] = origTalk[key + suffix];
+        }
+      }
+      if (talk.video) {
+        const duration = talk.duration.split(':')[0].replace(/^0/, '');
 
-    sectionContent += `<details class=talk><summary><div class="grid"><a href="talks/${slug}.html"><img src="${talk.thumbnail}" alt="Watch ${talk.title}" width=200 class="tn"></a><a href="talks/${slug}.html">${talk.title}</a><span class="summary"> by ${talk.author}${talk.affiliation ? " (" + talk.affiliation + ")" : ""} - ${duration} min <span></span></span></div>${talk.added ? "<span class=added>Added on " + talk.added + "</span>": ""}</summary>`;
-    sectionContent += `<p><a href="talks/${slug}.html">${duration} minutes presentation</a></p>`;
-  } else {
-    sectionContent += `<details><summary><div class="grid"><span>PENDING </span><a>${talk.title}</a><span class="summary"> by ${talk.author}${talk.affiliation ? " (" + talk.affiliation + ")" : ""} <span></span></span></div></summary>`;
-  }
-  sectionContent += `<dl>`;
-  sectionContent += `<dt>Speaker</dt>`;
-  sectionContent += `<dd>${talk.author}${talk.affiliation ? " (" + talk.affiliation + ")" : ""}</dd>`;
-  if (talk.bio) {
-    sectionContent += `<dd>${talk.bio}</dd>`;
-  }
-  if (talk.abstract) {
-    sectionContent += `<dt>Abstract</dt>`;
-    sectionContent += `<dd>${talk.abstract}</dd>`;
-  }
-  sectionContent += "</dl></details>\n"; 
-
-  if (!talk.video) continue;
-  const format = talk.format || "pdf";
-  const slidesurl = talk.noslide ? "" : talk.url || 'https://www.w3.org/2020/Talks/mlws/' + shortname + '.pdf';
-  let nextTalk;
-  let cur = i;
-  let sectionCur = sectionNum - 1;
-  while (true) {
-    cur++;
-    if (cur < Object.keys(talks[sectionCur]).length ) {
-      nextTalk = talks[sectionCur][Object.keys(talks[sectionCur])[cur]];
-      if (nextTalk && nextTalk.video) break;
-    } else if ( sectionCur < talks.length - 1) {
-      sectionCur++;
-      cur = -1;
-    } else {
-      break
+        sectionContent[lang] += `<details class=talk><summary><div class="grid"><a href="talks/${slug}.html"><img src="${talk.thumbnail}" alt="Watch ${talk.title}" width=200 class="tn"></a><a href="talks/${slug}.html">${talk.title}</a><span class="summary"> by ${talk.author}${talk.affiliation ? " (" + talk.affiliation + ")" : ""} - ${duration} min <span></span></span></div>${talk.added ? "<span class=added>Added on " + talk.added + "</span>": ""}</summary>`;
+        sectionContent[lang] += `<p><a href="talks/${slug}.html">${duration} minutes presentation</a></p>`;
+      } else {
+        sectionContent[lang] += `<details><summary><div class="grid"><span>PENDING </span><a>${talk.title}</a><span class="summary"> by ${talk.author}${talk.affiliation ? " (" + talk.affiliation + ")" : ""} <span></span></span></div></summary>`;
+      }
+      sectionContent[lang] += `<dl>`;
+      sectionContent[lang] += `<dt>Speaker</dt>`;
+      sectionContent[lang] += `<dd>${talk.author}${talk.affiliation ? " (" + talk.affiliation + ")" : ""}</dd>`;
+      if (talk.bio) {
+        sectionContent[lang] += `<dd>${talk.bio}</dd>`;
+      }
+      if (talk.abstract) {
+        sectionContent[lang] += `<dt>Abstract</dt>`;
+        sectionContent[lang] += `<dd>${talk.abstract}</dd>`;
+      }
+      sectionContent[lang] += "</dl></details>\n";
     }
-  }
-  const nextlink =  nextTalk ? toSlug(nextTalk.title) + ".html" : "";
-  const nexttitle = nextTalk ? nextTalk.title : "";
-  const duration_so = "PT" + talk.duration.split(":")[0] + "M" + talk.duration.split(":")[1] + "S";
-  const content = `---
+    const talk = origTalk;
+    if (!talk.video) continue;
+    const format = talk.format || "pdf";
+    const slidesurl = talk.noslide ? "" : talk.url || 'https://www.w3.org/2020/Talks/mlws/' + shortname + '.pdf';
+    let nextTalk;
+    let cur = i;
+    let sectionCur = sectionNum - 1;
+    while (true) {
+      cur++;
+      if (cur < Object.keys(talks[sectionCur]).length ) {
+        nextTalk = talks[sectionCur][Object.keys(talks[sectionCur])[cur]];
+        if (nextTalk && nextTalk.video) break;
+      } else if ( sectionCur < talks.length - 1) {
+        sectionCur++;
+        cur = -1;
+      } else {
+        break
+      }
+    }
+    const nextlink =  nextTalk ? toSlug(nextTalk.title) + ".html" : "";
+    const nexttitle = nextTalk ? nextTalk.title : "";
+    const duration_so = "PT" + talk.duration.split(":")[0] + "M" + talk.duration.split(":")[1] + "S";
+    const content = `---
 title: "${talk.title}"
 author: ${talk.author} (${talk.affiliation})
 layout: talk
@@ -66,7 +77,7 @@ thumbnailurl: ${talk.thumbnail}
 videourl: ${talk.video}
 slidesurl: ${slidesurl}
 ` + (talk.nosync ? `nosync: ${talk.nosync}\n` : '') +
-`prevlink: ${prevlink}
+          `prevlink: ${prevlink}
 prevtitle: "${prevtitle}"
 nextlink: ${nextlink}
 nexttitle: "${nexttitle}"
@@ -74,11 +85,12 @@ duration_so: "${duration_so}"
 ---
 `;
 
-  fs.writeFileSync("talks/" + slug + ".html", content, {encoding: "utf-8"});
-  prevlink = toSlug(talk.title) + ".html";
-  prevtitle = talk.title;
-}
-  sectionContent += "</dl>";
-  fs.writeFileSync("_includes/talk-list" + sectionNum + ".html", sectionContent, {encoding: "utf-8"});
-
+    fs.writeFileSync("talks/" + slug + ".html", content, {encoding: "utf-8"});
+    prevlink = toSlug(talk.title) + ".html";
+    prevtitle = talk.title;
+  }
+  for (let [lang, suffix] of Object.entries(languages)) {
+    sectionContent[lang] += "</dl>";
+    fs.writeFileSync("_includes/talk-list" + sectionNum + suffix + ".html", sectionContent[lang], {encoding: "utf-8"});
+  }
 }
